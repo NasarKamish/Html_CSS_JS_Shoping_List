6feaeac4eeab9776ca46719b01f4b1d2df8006b3
NasarKamish <Nasarkamish@gmail.com>
2021-08-13 16:13:52 +0200

    Updated file

--- scripts/shopList.js
+++ scripts/shopList.js
@@ -27,10 +27,38 @@ function addToCart(index) {
   cart.push(index);
   //   console.log(cart);
   let add_btn = document.querySelector(`.btn-Add-${index}`);
-  console.log(add_btn);
+  //   console.log(add_btn);
   add_btn.style.display = "none";
+  populateCart();
 }
 
 function showCart() {
   document.querySelector(".cart").classList.toggle("active");
+}
+
+function populateCart() {
+  console.log("awe");
+  fetch("http://127.0.0.1:5000/get-products/").then((request) => {
+    request.json().then((obj) => {
+      //   console.log(obj);
+      data = obj.data;
+      let cart_container = document.querySelector(".cart");
+      //   let total = 0;
+      cart_container.innerHTML = ``;
+      cart.forEach((order) => {
+        // console.log(order);
+        data.forEach((product) => {
+          if (product[0] == order) {
+            // console.log(product);
+            cart_container.innerHTML += `<div class="cart-item">
+            <p class="id">${product[0]}</p>
+            <p class="name">${product[1]}</p>
+            <p class="price">${product[2]}</p>
+            <p class="quantity">1</p>
+          </div>`;
+          }
+        });
+      });
+    });
+  });
 }
