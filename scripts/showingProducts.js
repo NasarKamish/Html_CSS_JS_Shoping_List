8c1ff2a3f3926d397b332663dbfdb4e8f5c2c7b4
NasarKamish <Nasarkamish@gmail.com>
2021-08-16 09:48:33 +0200

    Updated file

--- scripts/showingProducts.js
+++ scripts/showingProducts.js
@@ -2,32 +2,31 @@
 
 function showEdit(index) {
   console.log(index);
-  let display = "";
   if (index == -1) {
-    display = "none";
+    let edit_container = document.querySelector(".section-edit");
+    edit_container.style.display = "none";
   } else {
-    display = "flex";
-  }
-  let edit_container = document.querySelector(".section-edit");
-  edit_container.style.display = display;
-  let old_container = document.querySelector(".old-info");
-  fetch("http://127.0.0.1:5000/get-products/").then((request) => {
-    request.json().then((obj) => {
-      console.log(obj);
-      data = obj.data;
-      console.log(data[index]);
-      product = data[index];
-      old_container.innerHTML = `<div class="item">
-         <p class="product-name">Name: ${product[1]}</p>
-         <p class="product-price">Price: ${product[2]}</p>
-         <p class="product-date">Date: ${product[3]}</p> 
-     </div>`;
-      //  button with index for delete
+    let edit_container = document.querySelector(".section-edit");
+    edit_container.style.display = "flex";
+    let old_container = document.querySelector(".old-info");
+    fetch("http://127.0.0.1:5000/get-products/").then((request) => {
+      request.json().then((obj) => {
+        console.log(obj);
+        data = obj.data;
+        console.log(data[index]);
+        product = data[index];
+        old_container.innerHTML = `<div class="item">
+           <p class="product-name">Name: ${product[1]}</p>
+           <p class="product-price">Price: ${product[2]}</p>
+           <p class="product-date">Date: ${product[3]}</p> 
+       </div>`;
+        //  button with index for delete
+      });
     });
-  });
+  }
 }
 
-// Fetching poducts and displaying some
+// Fetching poducts and displaying them
 
 let container = document.querySelector(".container");
 
