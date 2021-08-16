f368366330195b3c4902b3ec81fc931c72648144
NasarKamish <Nasarkamish@gmail.com>
2021-08-16 10:05:00 +0200

    Updated file

--- scripts/showingProducts.js
+++ scripts/showingProducts.js
@@ -1,7 +1,19 @@
+// Delete function
+
+function deleteProduct(index) {
+  console.log(index);
+  // http://127.0.0.1:5000/delete-product/${index}/
+  let delConfirm = confirm("Are you sure you want to delete this product?");
+  if (delConfirm) {
+    fetch(`http://127.0.0.1:5000/delete-product/${index}/`);
+    createCards();
+  }
+}
+
 // Edit function
 
 function showEdit(index) {
-  console.log(index);
+  // console.log(index);
   if (index == -1) {
     let edit_container = document.querySelector(".section-edit");
     edit_container.style.display = "none";
@@ -11,9 +23,9 @@ function showEdit(index) {
     let old_container = document.querySelector(".old-info");
     fetch("http://127.0.0.1:5000/get-products/").then((request) => {
       request.json().then((obj) => {
-        console.log(obj);
+        // console.log(obj);
         data = obj.data;
-        console.log(data[index]);
+        // console.log(data[index]);
         product = data[index];
         old_container.innerHTML = `<div class="item">
            <p class="product-name">Name: ${product[1]}</p>
@@ -28,26 +40,30 @@ function showEdit(index) {
 
 // Fetching poducts and displaying them
 
-let container = document.querySelector(".container");
+function createCards() {
+  let container = document.querySelector(".container");
 
-fetch("http://127.0.0.1:5000/get-products/").then((request) => {
-  request.json().then((obj) => {
-    console.log(obj);
-    data = obj.data;
-    console.log(data);
-    container.innerHTML = ``;
-    let index = 0;
-    data.forEach((product) => {
-      container.innerHTML += `<div class="item">
+  fetch("http://127.0.0.1:5000/get-products/").then((request) => {
+    request.json().then((obj) => {
+      // console.log(obj);
+      data = obj.data;
+      // console.log(data);
+      container.innerHTML = ``;
+      let index = 0;
+      data.forEach((product) => {
+        container.innerHTML += `<div class="item">
          <img src="https://picsum.photos/200/200?random=${product[0]}" alt="${product[1]}">
          <p class="product-name">Name: ${product[1]}</p>
          <p class="product-price">Price: ${product[2]}</p>
          <p class="product-date">Date: ${product[3]}</p>
          <button onclick="showEdit(${index})" class="btn-edit">Edit</button>
-         <button class="btn-delete">Delete</button>
+         <button onclick="deleteProduct(${product[0]})" class="btn-delete">Delete</button>
      </div>`;
-      console.log(index);
-      index++;
+        // console.log(index);
+        index++;
+      });
     });
   });
-});
+}
+
+createCards();
