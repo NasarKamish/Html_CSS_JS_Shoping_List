a62b033d9b585c69302c707b2bda3c5b00810514
NasarKamish <Nasarkamish@gmail.com>
2021-08-17 14:18:23 +0200

    Updated file

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,15 @@ function showRegister() {
 }
 
 // Fetching users
-
-fetch("http://127.0.0.1:5000/get-users/").then((request) => {
-  request.json().then((obj) => {
-    data = obj.data;
-    console.log(data);
+function logIn() {
+  console.log("awe");
+  fetch("http://127.0.0.1:5000/get-users/").then((request) => {
+    request.json().then((obj) => {
+      data = obj.data;
+      console.log(data);
+    });
   });
-});
+}
 
 // Fetching users
 // can be used for log in
@@ -60,48 +62,3 @@ fetch("http://127.0.0.1:5000/get-users/").then((request) => {
 //         console.log(data)
 //     })
 // })
-
-// user register
-
-function userRegister() {
-  // Geting the values in the inputs into a dictionary
-  let inputs = document.querySelectorAll(".register-input");
-
-  let json_info = [];
-  let val_space = true;
-  inputs.forEach((input) => {
-    if (input.value == "") {
-      val_space = false;
-    }
-    json_info.push(input.value);
-  });
-
-  if (val_space) {
-    let formData = new FormData();
-    formData.append("first_name", json_info[0]);
-    formData.append("last_name", json_info[1]);
-    formData.append("Email", json_info[2]);
-    formData.append("username", json_info[3]);
-    formData.append("password", json_info[4]);
-
-    let json_dict = {
-      first_name: json_info[0],
-      last_name: json_info[1],
-      Email: json_info[2],
-      username: json_info[3],
-      password: json_info[4],
-    };
-    console.log(formData);
-    console.log(json_dict);
-    location.href = "";
-    fetch("http://127.0.0.1:5000/user-registration/", {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(json_dict),
-    });
-  } else {
-    alert("Please enter your details!");
-  }
-}
